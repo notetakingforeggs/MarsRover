e05bf710bba4f819416904b7394d140b9dce8cf0
notetakingforeggs <notetakingforeggs@gmail.com>
2024-05-16 12:10:55 +0100

    Feat: passing test for inputting non numeric

--- src/main/java/PlateauSizeParser.java
+++ src/main/java/PlateauSizeParser.java
@@ -1,14 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class PlateauSizeParser implements InstructionParser<String, PlateauSize>{
+public class PlateauSizeParser implements InstructionParser<String, PlateauSize> {
     @Override
     public PlateauSize parseInput(String input) {
 
         PlateauSize plateauSize = new PlateauSize();
+
+
         String[] dims = input.split(" ");
-        plateauSize.setWidth(Integer.parseInt(dims[0]));
-        plateauSize.setLength(Integer.parseInt(dims[1]));
+
+
+        try {
+            plateauSize.setWidth(Integer.parseInt(dims[0]));
+            plateauSize.setLength(Integer.parseInt(dims[1]));
+
+        } catch (NumberFormatException e) {
+            System.out.println("plateau size takes input of only numbers");
+        }
 
         return plateauSize;
     }
--- src/test/java/PlateauSizeParserTest.java
+++ src/test/java/PlateauSizeParserTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlateauSizeParserTest {
@@ -7,10 +8,17 @@ class PlateauSizeParserTest {
     @Test
     void parseInputTest() {
         PlateauSizeParser plateauSizeParser = new PlateauSizeParser();
+        PlateauSize testPlateau = new PlateauSize();
+        testPlateau.setWidth(4);
+        testPlateau.setLength(3);
 
-        assertAll(()-> {
+        assertAll(() -> {
 
-            assertInstanceOf( PlateauSize.class, plateauSizeParser.parseInput("1 1"));
+            assertInstanceOf(PlateauSize.class, plateauSizeParser.parseInput("1 1"));
+            assertInstanceOf(PlateauSize.class, plateauSizeParser.parseInput("m"));
+
+            assertEquals(4, plateauSizeParser.parseInput(("4 3")).getWidth());
+            assertThat(testPlateau).usingRecursiveComparison().isEqualTo(plateauSizeParser.parseInput(("4 3")));
 
         });
 
