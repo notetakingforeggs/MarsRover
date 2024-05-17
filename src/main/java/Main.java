6b89231aa34450176469eac8ed658db0f323cf34
notetakingforeggs <notetakingforeggs@gmail.com>
2024-05-17 15:44:02 +0100

    Feat: framework of reading file into program started

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<String> lines = new ArrayList<>();
+        String filepath = "/home/ludic/northcoders/fundamentals/MarsRover/src/main/resources/input.csv";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+                String line = reader.readLine();
+                while (line != null){
+                    lines.add(line);
+                    line = reader.readLine();
+                }
 
+        } catch (IOException e) {
+            System.out.println("no file?");
+        }
+lines.stream().forEach(System.out::println);
 
     }
 }
